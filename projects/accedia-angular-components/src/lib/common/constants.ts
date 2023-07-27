e6984c70bb1c3b5d193d769d6fd410921fa472ce
Hristiyan Stefanov <hristiyan.stefanov@accedia.com>
2023-07-27 15:35:52 +0300

    loading spinner component

--- projects/accedia-angular-components/src/lib/common/constants.ts
+++ projects/accedia-angular-components/src/lib/common/constants.ts
@@ -5,6 +5,7 @@ export const VARIANT_FILLED = 'filled';
 export const VARIANT_OUTLINED = 'outlined';
 
 // Generic sizing properties
+export const SIZE_EXTRA_SMALL = 'extra-small';
 export const SIZE_SMALL = 'small';
 export const SIZE_MEDIUM = 'medium';
 export const SIZE_LARGE = 'large';
--- projects/accedia-angular-components/src/lib/common/types.ts
+++ projects/accedia-angular-components/src/lib/common/types.ts
@@ -1,1 +1,8 @@
+import { SIZE_EXTRA_SMALL, SIZE_LARGE, SIZE_MEDIUM, SIZE_SMALL } from './constants';
+
 export type InlineStyle = { [key: string]: string };
+export type LoadingSpinnerSize =
+  | typeof SIZE_EXTRA_SMALL
+  | typeof SIZE_SMALL
+  | typeof SIZE_MEDIUM
+  | typeof SIZE_LARGE;
