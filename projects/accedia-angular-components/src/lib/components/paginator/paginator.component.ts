ab4a6bc2aa4375b0db7062271b3128a99aa6f5fc
Hristiyan Stefanov <stefanovh@aforbes.com>
2024-02-20 13:27:45 +0200

    implement paginator v2

--- projects/accedia-angular-components/src/lib/components/paginator/paginator.component.ts
+++ projects/accedia-angular-components/src/lib/components/paginator/paginator.component.ts
@@ -1,37 +1,174 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { BreakpointService } from '../../services/breakpoint.service';
-import { staggerFadeIn } from '../../common/animation.constants';
+
+export interface PageChangeEvent {
+  pageIndex: number,
+  pageSize: number,
+  itemsLength: number
+};
+
+const NO_ITEMS_LABEL: string = '0 of 0';
+const MIN_EFFECTIVE_PAGE_BUTTONS_COUNT = 3;
+const DEFAULT_PAGE_BUTTONS_COUNT = 5;
+const DEFAULT_PAGE_SIZE = 10;
+const MIN_PAGE_SIZE = 1;
 
 @Component({
   selector: 'acc-paginator',
   templateUrl: './paginator.component.html',
-  styleUrls: ['./paginator.component.scss'],
-  animations: [staggerFadeIn]
+  styleUrls: ['./paginator.component.scss']
 })
 export class PaginatorComponent {
-  @Input() pageNumber: number = 1;
-  @Input() totalPages: number;
-  @Output() pageNumberChange = new EventEmitter<number>();
+  @Input() disabled: boolean = false;
+  @Input() hidePageButtons: boolean = false;
+  @Input() itemsLength: number = 0;
+  @Input() pageIndex = 0;
+  @Input() showFirstLastButtons: boolean = true;
+  @Input() showPageSize: boolean = true;
+  @Input() showRange: boolean = true;
 
-  constructor(public breakpointService: BreakpointService) {}
+  @Input()
+  get pageButtonsCount(): number {
+    return this._pageButtonsCount;
+  }
+  set pageButtonsCount(count: number) {
+    this._pageButtonsCount = Math.max(count, MIN_EFFECTIVE_PAGE_BUTTONS_COUNT);
+  }
+
+  @Input() 
+  get pageSize(): number {
+    return this._pageSize;
+  }
+  set pageSize(size: number) {
+    this._pageSize = Math.max(size, MIN_PAGE_SIZE);
+  }
+  
+  @Input()
+  get pageSizeOptions(): number[] | undefined {
+    return this._pageSizeOptions;
+  }
+  set pageSizeOptions(pagesArray: number[] | undefined) {
+    this._pageSizeOptions = pagesArray?.filter(n => n >= MIN_PAGE_SIZE);
+  }
+
+  @Output() pageChange = new EventEmitter<PageChangeEvent>();
+
+  get numberOfPages(): number {
+    return Math.ceil(this.itemsLength / this.pageSize);
+  }
 
   get hasPrevious(): boolean {
-    return this.pageNumber > 1;
+    return this.pageIndex > 0;
   }
 
   get hasNext(): boolean {
-    return this.pageNumber < this.totalPages;
+    return (this.pageIndex + 1) < this.numberOfPages;
   }
 
-  previous() {
-    if (this.hasPrevious) {
-      this.pageNumberChange.emit(--this.pageNumber);
+  private _pageButtonsCount: number = DEFAULT_PAGE_BUTTONS_COUNT;
+  private _pageSize: number;
+  private _pageSizeOptions?: number[];
+  
+  ngOnInit(): void {
+    if (this.pageSizeOptions && this.pageSizeOptions.length > 0) {
+      this._pageSize = this.pageSizeOptions[0];
+      this._emitChanges();
     }
   }
 
-  next() {
+  changePageSize(changeEvent: any) {
+    const newPageSize = changeEvent.target.value ?? DEFAULT_PAGE_SIZE;
+    const currentFirstItemIndex = this.pageIndex * this.pageSize;
+    this.pageIndex = Math.floor(currentFirstItemIndex / newPageSize);
+    this.pageSize = newPageSize;
+    this._emitChanges();
+  }
+
+  getRangeLabel(): string {
+    if (this.itemsLength <= 0) {
+      return NO_ITEMS_LABEL;
+    }
+
+    const firstOfPage = this.pageIndex * this.pageSize + 1;
+    const lastOfPage = Math.min((this.pageIndex + 1) * this.pageSize, this.itemsLength);
+
+    return `${firstOfPage} – ${lastOfPage} of ${this.itemsLength}`;
+  }
+
+  goToNext() {
     if (this.hasNext) {
-      this.pageNumberChange.emit(++this.pageNumber);
+      this.pageIndex++;
+      this._emitChanges();
     }
+  }
+
+  goToPrevious() {
+    if (this.hasPrevious) {
+      this.pageIndex--;
+      this._emitChanges();
+    }
+  }
+
+  goToFirst() {
+    if (this.hasPrevious) {
+      this.pageIndex = 0;
+      this._emitChanges();
+    }
+  }
+
+  goToLast() {
+    if (this.hasNext) {
+      this.pageIndex = this.numberOfPages - 1;
+      this._emitChanges();
+    }
+  }
+
+  goTo(page: number) {
+    if (page > 0 && page <= this.numberOfPages) {
+      this.pageIndex = page - 1;
+      this._emitChanges();
+    }
+  }
+
+  getPageNumbersInRange(): number[] {
+    const currentPage = this.pageIndex + 1;
+    const pagesArray: number[] = [this.pageIndex + 1];
+
+    let precedingMargin = 1;
+    let succeedingMargin = 1;
+
+    for (let i = 1; i <= Math.floor(this.pageButtonsCount / 2); i++) {
+      if (currentPage - precedingMargin > 0) {
+        pagesArray.unshift(currentPage - precedingMargin);
+        precedingMargin++;
+      } else if (currentPage + succeedingMargin <= this.numberOfPages) {
+        pagesArray.push(currentPage + succeedingMargin);
+        succeedingMargin++;
+      }
+      if (currentPage + succeedingMargin <= this.numberOfPages) {
+        pagesArray.push(currentPage + succeedingMargin);
+        succeedingMargin++;
+      } else if (currentPage - precedingMargin > 0) {
+        pagesArray.unshift(currentPage - precedingMargin);
+        precedingMargin++;
+      }
+    }
+
+    if (this.pageButtonsCount % 2 === 0) {
+      if (pagesArray[pagesArray.length - 1] < this.numberOfPages && this.pageButtonsCount > MIN_EFFECTIVE_PAGE_BUTTONS_COUNT) {
+        pagesArray.pop();
+      } else if (pagesArray.length > this.pageButtonsCount) {
+        pagesArray.shift();
+      }
+    }
+
+    return pagesArray;
+  }
+
+  private _emitChanges() {
+    this.pageChange.emit({
+      pageIndex: this.pageIndex,
+      pageSize: this.pageSize,
+      itemsLength: this.itemsLength
+    });
   }
 }
--- projects/accedia-angular-components/src/stories/Paginator.stories.ts
+++ projects/accedia-angular-components/src/stories/Paginator.stories.ts
@@ -1,7 +1,6 @@
 import { type Meta, type StoryObj } from '@storybook/angular';
 import { PaginatorComponent } from '../lib/components/paginator/paginator.component';
 
-// More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
 const meta: Meta<PaginatorComponent> = {
   title: 'Components/Paginator',
   component: PaginatorComponent,
@@ -11,15 +10,55 @@ const meta: Meta<PaginatorComponent> = {
     },
   }),
   argTypes: {
-    pageNumber: {
-      name: 'pageNumber?',
-      description: 'Sets the current page number',
-      control: 'number'
+    disabled: {
+      name: 'disabled',
+      description: 'Paginator buttons disabled state',
+      control: 'boolean'
     },
-    totalPages: {
-      name: 'totalPages',
+    hidePageButtons: {
+      name: 'hidePageButtons',
+      description: 'Toggles the visibility of the numeric page buttons',
+      control: 'boolean'
+    },
+    itemsLength: {
+      name: 'itemsLength',
       description: 'The total number of pages',
       control: 'number'
+    },
+    pageIndex: {
+      name: 'pageIndex',
+      description: 'Sets the current page zero-based index',
+      control: 'number'
+    },
+    showFirstLastButtons: {
+      name: 'showFirstLastButtons',
+      description: 'Toggles the visibility of the first/last page buttons',
+      control: 'boolean'
+    },
+    showPageSize: {
+      name: 'showPageSize',
+      description: 'Toggles the visibility of the items per page data and select menu',
+      control: 'boolean'
+    },
+    showRange: {
+      name: 'showRange',
+      description: 'Toggles the visibility of the current items range',
+      control: 'boolean'
+    },
+    pageButtonsCount: {
+      name: 'pageButtonsCount',
+      description: 'The maximum number of page buttons visible at once. Default is 5, minimum is 3',
+      control: 'number'
+    },
+    pageSize: {
+      name: 'pageSize',
+      description: 'The items size of a single page if no page size options are specified. Default is 10',
+      control: 'number'
+    },
+    pageSizeOptions: {
+      name: 'pageSizeOptions',
+      description: 'An array of numbers containing the page size options that the user can select',
+      control: 'object'
     }
   },
 };
@@ -27,10 +66,17 @@ const meta: Meta<PaginatorComponent> = {
 export default meta;
 type Story = StoryObj<PaginatorComponent>;
 
-// More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
 export const Primary: Story = {
   args: {
-    pageNumber: 1,
-    totalPages: 5
+    disabled: false,
+    hidePageButtons: false,
+    itemsLength: 150,
+    pageIndex: 0,
+    showFirstLastButtons: true,
+    showPageSize: true,
+    showRange: true,
+    pageButtonsCount: 5,
+    pageSize: undefined,
+    pageSizeOptions: [10, 20, 30]
   },
 };
